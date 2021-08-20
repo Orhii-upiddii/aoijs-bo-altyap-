cd55229b7b86dd5d6f822210533e03f7ea3d737b
Orhii-upiddii <85016984+Orhii-upiddii@users.noreply.github.com>
2021-08-20 13:22:28 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const aoi = require('aoi.js')
+const aoi = new.aoiBot({
+   token: "process.env.token", //token env'e
+   prefix: "$getServerVar[prefix]" /// prefix icin biraz assagi in
+});
+
+bot.onMessage()   //Dokunmayın Yoksa Kodlar Çalışmaz
+
+bot.loadCommand("./komutlar/")
+
+bot.onReadyCommand({
+    channel:"" //hazirim kanal log 
+    code:`
+     $log[Başarıya Giriş Yapıldı]
+})
+
+
+
+bot.variables({
+   prefix:"" //prefixiniz
+
+////////////Kodlar Aşağıya////////////
+
+
+bot.command({
+  name:"eval",
+  code:`
+  $eval[$message]
+  $onlyForIDs[$botOwnerID;]
+` 
+})
+
